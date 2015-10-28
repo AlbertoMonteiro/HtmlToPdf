62021c533fa08aee7000dbc1734c0e4adf7ad6b4
Alberto Monteiro <alberto.monteiro@live.com>
2015-10-28 09:37:44 -0300

    Code commit

---
+++ Temp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Codaxy.WkHtmlToPdf;
+
+namespace HtmlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wkHtmlToPdfPath = Path.Combine(Environment.CurrentDirectory, "wkhtmltopdf", "wkhtmltopdf.exe");
+            PdfConvert.ConvertHtmlToPdf(new PdfDocument
+            {
+                Url = "http://wkhtmltopdf.org/"
+
+            }, new PdfConvertEnvironment { WkHtmlToPdfPath = wkHtmlToPdfPath, Timeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds }, new PdfOutput
+            {
+                OutputFilePath = "wkhtmltopdf-page.pdf"
+            });
+            Console.WriteLine("Done");
+        }
+    }
+}
